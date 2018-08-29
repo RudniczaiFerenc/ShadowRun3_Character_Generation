8884df38026a4e3f8ad69a8cd19ca5ce8afa028e
Feri <fckybtchz@gmail.com>
2018-08-29 21:00:14 +0200

    A few UI elements.

--- SR3_Chargen/SR3_Chargen/Form1.Designer.cs
+++ SR3_Chargen/SR3_Chargen/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SR3_Chargen
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBoxRace = new System.Windows.Forms.TextBox();
+            this.lblRace = new System.Windows.Forms.Label();
+            this.txtBoxSex = new System.Windows.Forms.TextBox();
+            this.txtBoxAge = new System.Windows.Forms.TextBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblAttributes = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(12, 50);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(157, 20);
+            this.txtBoxName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 34);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // txtBoxRace
+            // 
+            this.txtBoxRace.Location = new System.Drawing.Point(12, 89);
+            this.txtBoxRace.Name = "txtBoxRace";
+            this.txtBoxRace.Size = new System.Drawing.Size(89, 20);
+            this.txtBoxRace.TabIndex = 2;
+            // 
+            // lblRace
+            // 
+            this.lblRace.AutoSize = true;
+            this.lblRace.Location = new System.Drawing.Point(12, 73);
+            this.lblRace.Name = "lblRace";
+            this.lblRace.Size = new System.Drawing.Size(33, 13);
+            this.lblRace.TabIndex = 3;
+            this.lblRace.Text = "Race";
+            // 
+            // txtBoxSex
+            // 
+            this.txtBoxSex.Location = new System.Drawing.Point(107, 89);
+            this.txtBoxSex.Name = "txtBoxSex";
+            this.txtBoxSex.Size = new System.Drawing.Size(28, 20);
+            this.txtBoxSex.TabIndex = 4;
+            // 
+            // txtBoxAge
+            // 
+            this.txtBoxAge.Location = new System.Drawing.Point(141, 89);
+            this.txtBoxAge.Name = "txtBoxAge";
+            this.txtBoxAge.Size = new System.Drawing.Size(28, 20);
+            this.txtBoxAge.TabIndex = 5;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(107, 73);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(25, 13);
+            this.lblSex.TabIndex = 6;
+            this.lblSex.Text = "Sex";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(144, 73);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 7;
+            this.lblAge.Text = "Age";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(492, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(13, 131);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 9;
+            this.lblDescription.Text = "Description";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(492, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Location = new System.Drawing.Point(13, 170);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(35, 13);
+            this.lblNotes.TabIndex = 11;
+            this.lblNotes.Text = "Notes";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1054, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // lblAttributes
+            // 
+            this.lblAttributes.AutoSize = true;
+            this.lblAttributes.Location = new System.Drawing.Point(13, 231);
+            this.lblAttributes.Name = "lblAttributes";
+            this.lblAttributes.Size = new System.Drawing.Size(51, 13);
+            this.lblAttributes.TabIndex = 13;
+            this.lblAttributes.Text = "Attributes";
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1054, 517);
+            this.Controls.Add(this.lblAttributes);
+            this.Controls.Add(this.lblNotes);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.txtBoxAge);
+            this.Controls.Add(this.txtBoxSex);
+            this.Controls.Add(this.lblRace);
+            this.Controls.Add(this.txtBoxRace);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
             this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBoxRace;
+        private System.Windows.Forms.Label lblRace;
+        private System.Windows.Forms.TextBox txtBoxSex;
+        private System.Windows.Forms.TextBox txtBoxAge;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label lblAttributes;
     }
 }
 
